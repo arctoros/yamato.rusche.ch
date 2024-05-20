480a67ee23b3d1a62070c616b67d9ba9acd28c5c
Yamato <95149413+Yamato36@users.noreply.github.com>
2024-05-20 15:06:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-function vh(percent) {
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    return (percent * h) / 100;
+function calculate(q, r, type, classes, number, property) {
+    var s = 14.7;
+    var t = 25.04;
+    var sign = null;
+    var v = (q - r) / (s - t);
+    var p = (s * r - t * q) / (s - t);
+
+    v = v.toFixed(3);
+    p = p.toFixed(3);
+
+    if (type == "css") {
+        var elements = document.getElementsByClassName(classes);
+
+        if (p < 0) {
+            p = -p;
+            sign = "- ";
+        } else {
+            sign = "+ ";
+        }
+
+        if (number == null) {
+            number = elements.length;
+        }
+
+        for (var i = 0; i < number; i++) {
+            elements[i].style[property] = "calc(" + v + "vw " + sign + p + "px)";
+        }
+    } 
+    
+    else if (type == "n") {
+        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var result = v * w / 100 + parseInt(p);
+        return result;
+    }
 }
 
 window.addEventListener('scroll', function() {
-    var scrollArrow = document.getElementById('scrollArrow');
-    var threshold = vh(117) - 128;
+    var scrollArrowFixed = document.getElementsByClassName('scroll arrow fixed')[0];
+    var scrollArrowScrolling = document.getElementsByClassName('scroll arrow scrolling')[0];
+    var threshold = calculate(848, 1434, "n");
   
     if (window.scrollY >= threshold) {
-        scrollArrow.classList.add('scrolling');
+        scrollArrowFixed.style.display = "block";
+        scrollArrowScrolling.style.display = "none";
     } else {
-        scrollArrow.classList.remove('scrolling');
+        scrollArrowFixed.style.display = "none";
+        scrollArrowScrolling.style.display = "block";
     }
   });
 
@@ -37,15 +71,27 @@ var background = document.getElementsByClassName("nav background")[0];
 var open = false;
 
 navArrow.addEventListener("click", function() {
-    this.classList.toggle("active");
-
     if (open) {
-        container.style.right = "calc(-1 * var(--navwidth))";
-        background.style.right = "calc(-1 * var(--navwidth) + (100vw - var(--title_picture_width) - 60vw) / 2)";
+        this.style.transform = "translateX(0) rotate(0deg)";
+        container.style.width = "0vw";
+        background.style.width = "0vw";
         open = false;
     } else {
-        container.style.right = "-10vw";
-        background.style.right = "0vw";
+        this.style.transform = "translateX(-21vw) rotate(180deg)";
+        container.style.width = "25vw";
+        background.style.width = "35vw";
         open = true;
     }
 });
+
+// RESPOSIVENESS
+
+calculate(-1330, -940, "css", "scroll arrow scrolling", null, "top");
+calculate(-484, 492, "css", "scroll arrow fixed", null, "top");
+calculate(579, 921, "css", "scroll text", null, "top");
+calculate(235, 374, "css", "logo", null, "top");
+calculate(235, 374, "css", "title header", null, "top");
+calculate(336, 534, "css", "title subheader", null, "top");
+calculate(89, 103, "css", "title picture", null, "top");
+calculate(120, 153, "css", "nav container", null, "top");
+calculate(603, 1027, "css", "nav container", null, "height");
